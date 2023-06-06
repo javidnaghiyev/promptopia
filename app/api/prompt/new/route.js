d6b6b93084e6e8e7a7d97286cdb1031f920a386a
Cavid Nagiyev <cavid.nagiyev7@gmail.com>
2023-06-05 22:36:54 -0700

    Created profile

--- app/api/prompt/new/route.js
+++ app/api/prompt/new/route.js
@@ -1,10 +1,20 @@
-import { connectToDB } from "@utils/database"
+import Prompt from "@models/prompt";
+import { connectToDB } from "@utils/database";
 
 export const POST = async (req, res) => {
     const { prompt, tag, userId } = await req.json()
 
     try {
         await connectToDB()
+        const newPrompt = new Prompt({
+            creator: userId,
+            prompt,
+            tag
+        })
+
+        await newPrompt.save();
+
+        return new Response(JSON.stringify(newPrompt), { status: 201 })
     } catch (error) {
         console.log(error);
     }
